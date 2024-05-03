73567dc88c1bbae2dead939fc978ad9165d69d3e
Sertunc Selen <cahit.selen@nvi.gov.tr>
2024-05-03 16:23:58 +0300

    Add project files.

---
+++ YouTubeMusic.Api/Controllers/SearchController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace YouTubeMusic.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        //YouTubeMusic.Business
+        //YouTubeMusic.Business.Abstractions
+    }
+}
